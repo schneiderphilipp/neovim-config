be8ef50fc7dd9530ffa466ee5a410a57eeb71134
Philipp Schneider <philipp@philippschneider.at>
2024-05-17 10:01:22 +0200

    Initial commit

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,28 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- Lua
+        null_ls.builtins.formatting.stylua,
+
+        -- Javascript
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d"),
+        -- Python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+
+        -- Ruby
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
